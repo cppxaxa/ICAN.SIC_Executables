1861165ff85a8005f2864c95551d534486808ec5
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2019-09-20 01:16:04 +0530

    Automatic push

--- WebAssets/ChatInterface/js/RecordingButton.js
+++
@@ -1,9 +0,0 @@
-function StartRecordingAnimation(el){
-	$(el).removeClass("notRec");
-	$(el).addClass("Rec");
-}
-
-function EndRecordingAnimation(el){
-	$(el).removeClass("Rec");
-	$(el).addClass("notRec");
-}
